6bd39742b4a394ec29a1e6d03517fdece61d878e
allen-dharni <eklowlabs@outlook.com>
2022-03-25 17:53:20 +0800

    Automation Build We Cloud Dance

--- script/actuator/actuator.sh
+++ script/actuator/actuator.sh
@@ -22,7 +22,7 @@ echo "
 │   ╚███╔███╔╝███████╗    ╚██████╗███████╗╚██████╔╝╚██████╔╝██████╔╝    ██████╔╝██║  ██║██║ ╚████║╚██████╗███████╗           │
 │    ╚══╝╚══╝ ╚══════╝     ╚═════╝╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚══════╝           │
 │									   	 		  		   	     │
-│                  	     -- ☁️ 💃 WE CLOUD DANCE - MAKE EVERY ACTIONS ACCOUNTABLE. 🍻 🗽 --                               │
+│                  	     -- ☁️ 💃 WE CLOUD DANCE - MAKE EVERY ACTIONS ACCOUNTABLE. 🍻 🗽 --                                │
 │									   	 		  		   	     │
 │                             :: REMOTE USA FOR ANON SYS, Inc. - VERSION (V27.2.5-SNAPSHOT) ::                               │
 │									   	 		  		   	     │
